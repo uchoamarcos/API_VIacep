449226c4de15fd17a7a3523e7a310a0adbc4f06a
uchoamarcos <uchoamarcos307@gmail.com>
2026-08-05 21:21:25 -0300

    Remove preencheCep function from script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-async function preencheCep(input){
-  let cep = input.value.replace(/\D/g, '');
-  if (cep.length !== 8) return;
-  let response = await fetch(`https://viacep.com.br/ws/${cep}/json`);
-  let dados = await response.json();
-  if (dados.erro) return;
-  document.querySelector("#logradouro").value = dados.logradouro;
-  document.querySelector("#bairro").value = dados.bairro;
-  document.querySelector("#cidade").value = dados.localidade;
-  document.querySelector("#estado").value = dados.uf;
-}
+
